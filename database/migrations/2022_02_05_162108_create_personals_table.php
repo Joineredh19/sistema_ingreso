f5f15410d8462dffb1e1cda5e5dd2f4bd3da8153
browinlopez <browinsantiago@gmail.com>
2022-02-08 09:44:29 -0500

    Cambio del controlador personal

--- database/migrations/2022_02_05_162108_create_personals_table.php
+++ database/migrations/2022_02_05_162108_create_personals_table.php
@@ -16,12 +16,12 @@ class CreatePersonalsTable extends Migration
         Schema::create('personals', function (Blueprint $table) {
             $table->id();
             $table->string('T_identificacion',20);
-            $table->integer('N_identificacion');
-            $table->string('Nombre',10);
-            $table->string('Apellido',15);
-            $table->string('Correo',25);
-            $table->integer('Telefono');
-            $table->string('Cargo',15);
+            $table->string('N_identificacion',20);
+            $table->string('Nombre',50);
+            $table->string('Apellido',50);
+            $table->string('Correo',50);
+            $table->string('Telefono', 20);
+            $table->string('Cargo',50);
             $table->timestamps();
         });
     }
